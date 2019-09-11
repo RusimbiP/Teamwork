5fb19050b173cb8cfd59c5509214e3e40b1ffd36
RusimbiPatrick <rusimbipatrick@outlook.com>
2019-09-11 21:59:15 +0200

    feat(comment):use ES6 syntactic sugar in comment script [#168360287]

--- js/comment.js
+++ js/comment.js
@@ -2,16 +2,23 @@ const commentArea = document.querySelector('.form-control');
 const commentSection = document.querySelector('.comment-container');
 const publish = document.querySelector('.btn-default');
 
-commentArea.onclick = function() {
+const toggle = () => {
   commentSection.classList.toggle('toggled');
 }
 
-publish.onclick = function() {
+const untoggle = () => {
   commentSection.classList.remove('toggled');
 }
+
+publish.addEventListener('click', untoggle);
+commentArea.addEventListener('click', toggle);
 
 window.onclick = function(e) {
   if (e.target !== commentArea) {
     commentSection.classList.remove('toggled');
+    if(commentArea.value.length > 0){
+
+    }
   }
 }
+
