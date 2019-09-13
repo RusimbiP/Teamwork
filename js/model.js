51bccca66be1af58aa41bab74d7e4fda0c0deb6d
RusimbiPatrick <rusimbipatrick@outlook.com>
2019-09-13 09:26:56 +0200

    feat(edit-article):create a script to manage the visibility of the popu modal [#168420974]

---
+++ js/model.js
@@ -0,0 +1,22 @@
+const modal = document.getElementById('myModal');
+    const modalHeader = document.getElementsByClassName('modal-header');
+    const button = document.querySelectorAll('.btn');
+    const span = document.getElementsByClassName("close")[0];
+
+    const allButtons = Array.from(button);
+
+    allButtons.forEach(button =>{
+      button.onclick = () => {
+      modal.style.display = "block";
+    }
+    });
+
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+  
+    window.onclick = function(e) {
+      if (e.target == modal) {
+        modal.style.display = "none";
+      }
+    }
