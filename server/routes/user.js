696b8e51b32fe64d143c20813d140e48a9395070
RusimbiPatrick <rusimbipatrick@outlook.com>
2019-09-30 01:18:10 +0200

    feat(signup):create signup route [Finishes #168497662]

---
+++ server/routes/user.js
@@ -0,0 +1,31 @@
+import controller from '../controllers/user';
+const { create } = controller;
+import { 
+  checkFirstname, checkLastName, checkEmail, 
+  checkPassword, checkGender, checkJobRole,
+  checkDepartment, checkAddress
+}
+from '../helpers/validations';
+import errorHandler from '../Middleware/errorHandler';
+import { Router } from 'express';
+
+
+const userUrl = Router();
+
+
+userUrl.post('/signup', checkFirstname(), 
+checkLastName(), 
+checkEmail(),
+checkGender(), 
+checkJobRole(),
+checkDepartment(), 
+checkAddress(), 
+checkPassword(), 
+errorHandler, 
+create
+);
+
+
+
+
+export default userUrl;
