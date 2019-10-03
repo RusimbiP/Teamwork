78be45e6a38389e902e5e893509560ad40581a4f
RusimbiPatrick <rusimbipatrick@outlook.com>
2019-10-03 20:00:55 +0200

    feat(view-specific-article):add view specific article route [#168497669]

--- server/routes/article.js
+++ server/routes/article.js
@@ -1,12 +1,15 @@
 import { Router } from 'express';
 import controller from '../controllers/article';
+import { checkId } from '../helpers/validations';
 import errorHandler from '../middleware/errorHandler';
 import verifyToken from '../middleware/verifyToken';
 
-const { write } = controller;
+const { write, getOne} = controller;
 const articleUrl = Router();
 
 articleUrl.post('/', verifyToken, write);
+articleUrl.get('/:articleId',verifyToken, getOne);
+
 
 export default articleUrl;
 
