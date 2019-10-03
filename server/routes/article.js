2f373fa79c76758118d7c8fca57bc4fad162bbd3
RusimbiPatrick <rusimbipatrick@outlook.com>
2019-10-03 20:19:03 +0200

    feat(edit-article):add routes for editing article [ #168497672]

--- server/routes/article.js
+++ server/routes/article.js
@@ -4,12 +4,12 @@ import { checkId } from '../helpers/validations';
 import errorHandler from '../middleware/errorHandler';
 import verifyToken from '../middleware/verifyToken';
 
-const { write, getOne} = controller;
+const { write, getOne, edit } = controller;
 const articleUrl = Router();
 
 articleUrl.post('/', verifyToken, write);
 articleUrl.get('/:articleId',verifyToken, getOne);
-
+articleUrl.patch('/:articleId',verifyToken, edit);
 
 export default articleUrl;
 
