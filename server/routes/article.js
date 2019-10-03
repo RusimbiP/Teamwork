70bf7fec7fbce5ca96980315d5845000741ae8a6
RusimbiPatrick <rusimbipatrick@outlook.com>
2019-10-03 13:51:27 +0200

    feat(write-comment):add dummy article [#168497666]

--- server/routes/article.js
+++ server/routes/article.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import controller from '../controllers/article';
 import errorHandler from '../middleware/errorHandler';
 import verifyToken from '../middleware/verifyToken';
+
 const { write } = controller;
 const articleUrl = Router();
 
