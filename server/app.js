17fe1a84c593d0f93e55e8c183d0eb2031ecdb80
RusimbiPatrick <rusimbipatrick@outlook.com>
2019-10-04 01:52:06 +0200

    ready for heroku

--- server/app.js
+++ server/app.js
@@ -15,4 +15,19 @@ app.use('/api/v1/articles', articleUrl);
 app.use('/api/v1/articles/', commentUrl);
 app.get('/', (req, res) => res.send('Welcome to Teamwork'));
 
+app.use((req, res, next) => {
+  const error = new Error('Oh! snap! There is not such a page. Double check your url.');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    status: error.status || 500,
+    message: error.message
+  });
+  next();
+});
+
 export default app;
