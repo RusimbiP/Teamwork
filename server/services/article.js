d336bf0dc816a436310b88796499e4d2436bc486
RusimbiPatrick <rusimbipatrick@outlook.com>
2019-10-03 02:42:19 +0200

    feat(write-article):create article service [#168497665]

---
+++ server/services/article.js
@@ -0,0 +1,13 @@
+import article from '../models/article';
+
+export default class service {
+  static write(input, authorId){
+      const written = article.publish(input, authorId);
+      return {
+        status: 201,
+        message: `Article succesfully created`,
+        written
+      }
+  }
+}
+
