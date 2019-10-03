f50e1e83643a734ddf10906b1d1b250d88b158b0
RusimbiPatrick <rusimbipatrick@outlook.com>
2019-10-03 19:57:50 +0200

    feat(view-specific-article):add dummy data in models [#168497669]

--- server/models/article.js
+++ server/models/article.js
@@ -2,21 +2,21 @@
 class article {
   constructor() {
     this.articles = [
-    {
-      id: 1,
-      owner: 2,
-      title: "Restful APIs",
-      subtitle: "",
-      article: "Representational state transfer is a software architectural style that defines a set of constraints to be used for creating Web services. Web services that conform to the REST architectural style, called RESTful Web services, provide interoperability between computer systems on the Internet",
-      createdOn: "2019-10-03T11:07:23.423Z"
-    }
-  ];
+      {
+        id: 1,
+        authorId: 1,
+        title: 'Restful APIs',
+        subtitle: '',
+        article: 'Representational state transfer is a software architectural style that defines a set of constraints to be used for creating Web services. Web services that conform to the REST architectural style, called RESTful Web services, provide interoperability between computer systems on the Internet',
+        createdOn: '2019-10-03T11:07:23.423Z',
+      },
+    ];
   }
 
   publish(input, authorId) {
     const newArticle = {
       id: this.articles.length + 1,
-      owner: authorId,
+      authorId,
       title: input.title.trim() || 'Untitled',
       subtitle: input.subtitle.trim(),
       article: input.article,
@@ -27,8 +27,10 @@ class article {
   }
 
   retriveArticle(id) {
-    return this.articles.find(art => art.id == id);
+    return this.articles.find((art) => art.id == id);
   }
+
+  
 }
 export default new article();
 
