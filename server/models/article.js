135e2b48a1e2e299f122707506b3713062c8fb82
RusimbiPatrick <rusimbipatrick@outlook.com>
2019-10-03 13:49:34 +0200

    feat(write-comment):create comment model [ #168497666]

--- server/models/article.js
+++ server/models/article.js
@@ -1,7 +1,16 @@
 
 class article {
   constructor() {
-    this.articles = [];
+    this.articles = [
+    {
+      id: 1,
+      owner: 2,
+      title: "Restful APIs",
+      subtitle: "",
+      article: "Representational state transfer is a software architectural style that defines a set of constraints to be used for creating Web services. Web services that conform to the REST architectural style, called RESTful Web services, provide interoperability between computer systems on the Internet",
+      createdOn: "2019-10-03T11:07:23.423Z"
+    }
+  ];
   }
 
   publish(input, authorId) {
@@ -15,6 +24,10 @@ class article {
     };
     this.articles.push(newArticle);
     return newArticle;
+  }
+
+  retriveArticle(id) {
+    return this.articles.find(art => art.id == id);
   }
 }
 export default new article();
