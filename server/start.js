d8ba63d78acc01d5d732ecdb09c31398a8cdc414
RusimbiPatrick <rusimbipatrick@outlook.com>
2019-10-03 10:44:10 +0200

    feat(fix-validation):fix LF feed eslint problem [#168902935]

--- server/start.js
+++ server/start.js
@@ -1,5 +1,6 @@
 // Start our app!
 import app from './app';
+
 app.set('port', process.env.PORT || 3000);
 const server = app.listen(app.get('port'), () => {
   console.log(`Teamwork running. PORT ${server.address().port}`);
@@ -20,6 +21,3 @@ const server = app.listen(app.get('port'), () => {
                                   /-'Y'-\
                                 (__/ \__)
   */
-
-  
- 
