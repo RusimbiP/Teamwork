0427ef7775dec03abbcf97099bdc1d107765a4b7
RusimbiPatrick <rusimbipatrick@outlook.com>
2019-10-03 02:41:07 +0200

    feat(write-article):create article model [#168497665]

---
+++ server/models/article.js
@@ -0,0 +1,22 @@
+
+class article {
+  constructor() {
+    this.articles = [];
+  }
+
+  publish(input, authorId) {
+    const newArticle = {
+      id: this.articles.length +1,
+      owner:authorId,
+      title: input.title.trim() || 'Untitled',
+      subtitle: input.subtitle.trim(),
+      article: input.article,
+      createdOn: new Date(),
+    };
+    this.articles.push(newArticle);
+    return newArticle;
+  }
+}
+export default new article();
+
+
