d01224023710f62216161ca3150440c6330ab519
RusimbiPatrick <rusimbipatrick@outlook.com>
2019-10-07 11:27:20 +0200

    chore(setup-db):pepare terrain for db-intergration [Start #168977551]

---
+++ server/v2/start.js
@@ -0,0 +1,23 @@
+// Start our app!
+import app from './app';
+app.set('port', process.env.PORT || 3000);
+const server = app.listen(app.get('port'), () => {
+  console.log(`Teamwork running. PORT ${server.address().port}`);
+});
+  /**
+                                   .-"""-.
+                                / .===. \
+                                \/ 6 6 \/
+                                ( \___/ )
+    _________________________ooo__\_____/______________________________
+  /                                                                   \
+  | This project is Protected by Andela Honor code; third EPIC value |
+  \_______________________________________ooo_________________________/
+                                  |  |  |
+                                  |_ | _|
+                                  |  |  |
+                                  |__|__|
+                                  /-'Y'-\
+                                (__/ \__)
+  */
+ 
