8a92e352034a530c07fc3214ed92b2eda2beebfc
RusimbiPatrick <rusimbipatrick@outlook.com>
2019-10-07 20:24:34 +0200

    chore(setup-db):destructure data from dotenv [#168977551]

--- server/v2/db/migrations/seed.js
+++ server/v2/db/migrations/seed.js
@@ -9,20 +9,15 @@ const pool = new Pool({ connectionString: conn });
 pool.on('connect', () => {
   console.log('We connected to the database');
 });
+const {
+  firstname, lastname, email, password, gender, jobrole, deparment, address,
+} = process.env;
 
-const adminFirstname = process.env.firstname;
-const adminLastname = process.env.lastname;
-const adminEmail = process.env.email;
-const pass = process.env.pass;
-const adminGender = process.env.gender;
-const adminJobrole = process.env.jobrole;
-const adminDepartment = process.env.department;
-const adminAddress = process.env.address;
 
 const seed = async () => {
   const dataSeeds = [
-    `INSERT INTO users(firstname, lastname, email, password, gender, Jobrole, department, address, isAdmin) VALUES('John', 'Doe', 'taken@teamwork.com',${pass}, 'Male','Accountant', 'Finance', 'KG 232 Ave', 'false')`,
-    `INSERT INTO users(firstname, lastname, email, password, gender, jobrole, department, address,  isAdmin) VALUES(${adminFirstname}, ${adminLastname}, ${adminEmail}, ${pass}, ${adminGender},${adminJobrole}, ${adminDepartment}, ${adminAddress}, 'true')`,
+    `INSERT INTO users(firstname, lastname, email, password, gender, Jobrole, department, address, isAdmin) VALUES('John', 'Doe', 'taken@teamwork.com','${password}', 'Male','Accountant', 'Finance', 'KG 232 Ave', 'false')`,
+    `INSERT INTO users(firstname, lastname, email, password, gender, jobrole, department, address,  isAdmin) VALUES('${firstname}', '${lastname}', '${email}', '${password}', '${gender}','${jobrole}', '${deparment}', '${address}', 'true')`,
   ];
 
   for (const seeds of dataSeeds) {
