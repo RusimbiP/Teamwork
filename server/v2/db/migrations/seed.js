ed1bb534c9b80ed91c2a200a8f3e24dfcdfe30cd
RusimbiPatrick <rusimbipatrick@outlook.com>
2019-10-07 19:09:08 +0200

    chore(setup-db):remove duplication and hide sensitive information [#168977551]

--- server/v2/db/migrations/seed.js
+++ server/v2/db/migrations/seed.js
@@ -3,19 +3,26 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-let conn = conn = process.env.DATABASE_URL;
+const conn = process.env.DATABASE_URL;
 
 const pool = new Pool({ connectionString: conn });
 pool.on('connect', () => {
   console.log('We connected to the database');
 });
 
-
+const adminFirstname = process.env.firstname;
+const adminLastname = process.env.lastname;
+const adminEmail = process.env.email;
+const pass = process.env.pass;
+const adminGender = process.env.gender;
+const adminJobrole = process.env.jobrole;
+const adminDepartment = process.env.department;
+const adminAddress = process.env.address;
 
 const seed = async () => {
   const dataSeeds = [
-    'INSERT INTO users(firstname, lastname, email, password, gender, jobrole, department, address,  isAdmin) VALUES(\'John\', \'Doe\', \'taken@teamwork.com\',\'$2b$08$ByXvwUxxU47vy1tsKynZOecjXggYnsA4X9S3p2pWPKt1sRONu/P72\', \'Male\',\'Accountant\', \'Finance\', \'KG 232 Ave\', \'false\')',
-    'INSERT INTO users(firstname, lastname, email, password, gender, jobrole, department, address,  isAdmin) VALUES(\'Rusimbi\', \'Patrick\', \'admin@teamwork.com\',\'$2b$08$ByXvwUxxU47vy1tsKynZOecjXggYnsA4X9S3p2pWPKt1sRONu/P72\', \'Male\',\'Administor\', \'IT\', \'KG 232 Ave\', \'true\')',
+    `INSERT INTO users(firstname, lastname, email, password, gender, Jobrole, department, address, isAdmin) VALUES('John', 'Doe', 'taken@teamwork.com',${pass}, 'Male','Accountant', 'Finance', 'KG 232 Ave', 'false')`,
+    `INSERT INTO users(firstname, lastname, email, password, gender, jobrole, department, address,  isAdmin) VALUES(${adminFirstname}, ${adminLastname}, ${adminEmail}, ${pass}, ${adminGender},${adminJobrole}, ${adminDepartment}, ${adminAddress}, 'true')`,
   ];
 
   for (const seeds of dataSeeds) {
