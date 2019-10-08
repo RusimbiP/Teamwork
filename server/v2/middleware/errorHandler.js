24811433b67d74bafec53ccf523c3205486fa87a
RusimbiPatrick <rusimbipatrick@outlook.com>
2019-10-08 16:14:05 +0200

    feat(signup):add middleware to handle errors [#168977553]

---
+++ server/v2/middleware/errorHandler.js
@@ -0,0 +1,29 @@
+import { validationResult } from 'express-validator';
+import { log } from 'util';
+
+
+const errorHandler = (req, res, next) => {
+  const errors = {};
+
+  const errorFormatter = ({ location, msg, param }) => {
+   
+    if (!Object.keys(errors).includes(location)) {
+      errors[`${location}`] = {};
+    }
+    errors[`${location}`][`${param}`] = msg;
+
+    return errors;
+  };
+
+  const validationResults = validationResult(req).array({ onlyFirstError: true });
+
+  validationResults.forEach((resultObject) => errorFormatter(resultObject));
+
+  if (Object.keys(errors).length > 0) {
+    res.status(400).json({ status: 400, errors });
+  } else {
+    next();
+  }
+};
+
+export default errorHandler;
