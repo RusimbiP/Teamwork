6674986406a01766dcbb5610a8644e9bd0075d7a
RusimbiPatrick <rusimbipatrick@outlook.com>
2019-10-08 21:25:22 +0200

    feat(signin):create signup controller [#168977884]

--- server/v2/controllers/user.js
+++ server/v2/controllers/user.js
@@ -7,6 +7,11 @@
      const response = await service.create(req.body);
      res.status(response.status).json(response);
    }
+   /** **************** Controls Login ********************* */
+   static async login(req, res) {
+    const response = await service.login(req.body);
+    res.status(response.status).json(response);
+  }
  /** *********************END ****************************** */
  }
  
