6955b0f5c7bad8a119d028b6f1a3047718110875
RusimbiPatrick <rusimbipatrick@outlook.com>
2019-10-09 12:12:33 +0200

    feat(write-test):try to debug tests in travis [#168978258]

--- server/v2/tests/article.test.js
+++ server/v2/tests/article.test.js
@@ -1,11 +1,9 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import Helper from '../helpers/helper';
+const { Tokenize } = Helper;
 import app from '../app';
 import { mockUser, newUser, mockArticle, unregisteredEmail } from '../db/mockData';
-const { Tokenize } = Helper;
-
-const { email, password } = mockUser;
 
 
 const { expect } = chai;
@@ -16,18 +14,19 @@ describe('tests for comment and article endpoints', () => {
   let userToken;
   const invalid = 'eyJhbGciOiJIUz4230XBsb3llZUlkIjp7ImlkIjoxLCJmaXJzdE5hbWUiOiJjbWMiLCJXJ0bWVudCI6Im5ra24iLCJhZGRyZXNzIjoiS0cgMzQ0IFN0In0sImlhdCI6MTU3MDA2MTMxMywiZXhwIjoxNTcwNjY2MTEzfQ.lxPt4KGiDAan3U8PVdOK7eLRnIntGylHNgI14Mls7QY';
   const unregUser = Tokenize(unregisteredEmail);
-
+    
   before((done) => {
     chai
       .request(app)
       .post('/api/v2/auth/signin')
       .send({
-        'email': 'taken@teamwork.com',
-	      'password': 'password',
+        "email": "taken@teamwork.com",
+	      "password": "password"
       })
       .end((err, res) => {
         console.log(res.body);
-        userToken = res.body.data.token;
+        const { token } = res.body.data;
+        userToken = token;
         done(err);
       });
   });
@@ -100,19 +99,19 @@ describe('tests for comment and article endpoints', () => {
         });
     });
 
-    it('should not create an article if the user is not registered', (done) => {
-      chai
-        .request(app)
-        .post('/api/v2/articles')
-        .set('x-access-token', `${unregUser}`)
-        .send(mockArticle)
-        .end((err, res) => {
-          expect(res).to.have.status(403);
-          expect(res.body.status).to.be.equal(403);
-          expect(res.body.error).to.be.equal('You need to be registered to perform this action');
-          expect(res.body).to.be.an('object');
-          done(err);
-        });
-    });
+     it('should not create an article if the user is not registered', (done) => {
+       chai
+         .request(app)
+         .post('/api/v2/articles')
+         .set('x-access-token', `${unregUser }`)
+         .send(mockArticle)
+         .end((err, res) => {
+           expect(res).to.have.status(403);
+           expect(res.body.status).to.be.equal(403);
+           expect(res.body.error).to.be.equal('You need to be registered to perform this action');
+           expect(res.body).to.be.an('object');
+           done(err);
+         });
+     });
   });
 });
