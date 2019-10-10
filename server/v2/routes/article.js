82604de1e2de2a5af3499ff443e459812df32f5d
RusimbiPatrick <rusimbipatrick@outlook.com>
2019-10-10 10:21:18 +0200

    feat(edit-article):create edit article route [#168978363]

--- server/v2/routes/article.js
+++ server/v2/routes/article.js
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 import controller from '../controllers/article';
 import Auth from '../middleware/verifyToken';
+import { checkId } from '../helpers/validations';
+import errHandler from '../middleware/errorHandler';
 
-const { write } = controller;
+
+const { write, edit } = controller;
 const articleUrl = Router();
 
 articleUrl.post('/', Auth.verifyToken, write);
+articleUrl.patch('/:articleid', checkId(), errHandler, Auth.verifyToken, edit);
 
 export default articleUrl;
