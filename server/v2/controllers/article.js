ba68ffb334ae95bca3e7632207ee6bf453965c76
RusimbiPatrick <rusimbipatrick@outlook.com>
2019-10-10 10:20:51 +0200

    feat(edit-article):create edit article controller [#168978363]

--- server/v2/controllers/article.js
+++ server/v2/controllers/article.js
@@ -4,8 +4,13 @@ class controller {
   static async write(req, res){
     const response = await service.write(req.body, req.authorId);
     res.status(response.status).json(response);
-
   }
+
+  static async edit(req, res){
+    const response = await service.edit(req.body, req.params, req.authorId);
+    res.status(response.status).json(response);
+  }
+
 }
 
 export default controller;
