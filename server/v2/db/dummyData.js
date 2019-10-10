88037ff10386f8735cd07d34dc847f57e4bd6932
RusimbiPatrick <rusimbipatrick@outlook.com>
2019-10-10 17:35:08 +0200

    feat(delete-article):remove uwanted import [#168978369]

--- server/v2/db/dummyData.js
+++ server/v2/db/dummyData.js
@@ -1,5 +1,4 @@
 import { pool } from '../config/connection'
-import articleUrl from '../routes/article';
 
 pool.on('connect', () => {
   console.log('Creating test data...');
@@ -8,10 +7,8 @@ const { password } = process.env;
 
 
 const seed = async () => {
-  const userTestAccount = `INSERT INTO users(firstname, lastname, email, password, gender, Jobrole, department, address, isAdmin) VALUES('John', 'Doe', 'taken@teamwork.com','${password}', 'Male','Accountant', 'Finance', 'KG 232 Ave', 'false')`,
-  
+  const userTestAccount = `INSERT INTO users(firstname, lastname, email, password, gender, Jobrole, department, address, isAdmin) VALUES('John', 'Doe', 'taken@teamwork.com','${password}', 'Male','Accountant', 'Finance', 'KG 232 Ave', 'false')`;
   await pool.query(userTestAccount);
-
   console.log('Done.');
 }
 seed();
