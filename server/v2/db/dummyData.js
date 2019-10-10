e88bdb16fa5ea7578b4f05d9e8df13538918411a
RusimbiPatrick <rusimbipatrick@outlook.com>
2019-10-10 17:27:53 +0200

    feat(delete-article):remove unwanted query [#168978369]

--- server/v2/db/dummyData.js
+++ server/v2/db/dummyData.js
@@ -9,10 +9,8 @@ const { password } = process.env;
 
 const seed = async () => {
   const userTestAccount = `INSERT INTO users(firstname, lastname, email, password, gender, Jobrole, department, address, isAdmin) VALUES('John', 'Doe', 'taken@teamwork.com','${password}', 'Male','Accountant', 'Finance', 'KG 232 Ave', 'false')`,
-  testArticle = `INSERT INTO articles(title, subtitle, article, authorId) VALUES('title', 'subtitle', 'article', '1') returning *`
   
   await pool.query(userTestAccount);
-  await pool.query( articleTestAccount);
 
   console.log('Done.');
 }
