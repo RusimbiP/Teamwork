561442bfc934798d50a76a9eca2ed54598a1bc1c
RusimbiPatrick <rusimbipatrick@outlook.com>
2019-10-10 13:31:35 +0200

    feat(delete-article):insert a test article in test db [#168978369]

--- server/v2/db/dummyData.js
+++ server/v2/db/dummyData.js
@@ -1,4 +1,5 @@
 import { pool } from '../config/connection'
+import articleUrl from '../routes/article';
 
 pool.on('connect', () => {
   console.log('Creating test data...');
@@ -7,8 +8,12 @@ const { password } = process.env;
 
 
 const seed = async () => {
-  const userTestAccount = `INSERT INTO users(firstname, lastname, email, password, gender, Jobrole, department, address, isAdmin) VALUES('John', 'Doe', 'taken@teamwork.com','${password}', 'Male','Accountant', 'Finance', 'KG 232 Ave', 'false')`;
+  const userTestAccount = `INSERT INTO users(firstname, lastname, email, password, gender, Jobrole, department, address, isAdmin) VALUES('John', 'Doe', 'taken@teamwork.com','${password}', 'Male','Accountant', 'Finance', 'KG 232 Ave', 'false')`,
+  testArticle = `INSERT INTO articles(title, subtitle, article, authorId) VALUES('title', 'subtitle', 'article', '1') returning *`
+  
   await pool.query(userTestAccount);
+  await pool.query( articleTestAccount);
+
   console.log('Done.');
 }
 seed();
