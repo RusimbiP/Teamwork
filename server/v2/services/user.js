10cbacf72185ad894eab24ea37a54e6c3c67ed2a
RusimbiPatrick <rusimbipatrick@outlook.com>
2019-10-11 05:10:30 +0200

    bug(signin):fix status code on signin failure [Start #169077123]

--- server/v2/services/user.js
+++ server/v2/services/user.js
@@ -42,10 +42,10 @@ class service {
     try{
       const { rows } = await runQuery(queries.isRegistered, values);
       if (!rows[0]) {
-        return { status:400, error: 'Wrong email and password combination'}
+        return { status:401, error: 'Wrong email and password combination'}
       }
       if(!comparePassword(rows[0].password, credentials.password)){
-        return { status:400, error: 'Wrong email and password combination'}
+        return { status:401, error: 'Wrong email and password combination'}
       }
       const token = Tokenize(rows[0].email);
       return{ 
